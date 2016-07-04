09ce4bce4abca2fa86f3348a99f52fc1a54fe2cb
spyshiv <spyshiv@gmail.com>
2016-07-05 00:33:34 +0530

    Add test code for filter by tags

--- static/angular-module.js
+++ static/angular-module.js
@@ -2,12 +2,28 @@ var app = angular.module('Flickr', []);
 
 app.controller('FlickrController', ['$scope', '$http', function ($scope, $http) {
   	$scope.getPhotos = function (){
-  		var url = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=e06443c1e6342ce6b0c4f2d0247af627&extras=owner_name%2C+date_upload%2C+tags%2C+url_sq&per_page=500&page=4&format=json&nojsoncallback=1&auth_token=72157670609489975-464a212a57b686f2&api_sig=226442775acaa67e8759e8551062fb53";
+  		var url = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=e06443c1e6342ce6b0c4f2d0247af627&extras=owner_name%2C+date_upload%2C+tags%2C+url_sq%2C+date_taken&per_page=500&page=4&format=json&nojsoncallback=1&auth_token=72157670609489975-464a212a57b686f2&api_sig=79033250ebb7bef62a075d0eb3b0a585";
 	  	$http.get(url)
 	    .then(function(response) {
 	    	$scope.photodata = response.data.photos.photo;
-	    	console.log($scope.photodata);
+	    	var tags_set = [];
+	    	for (var i = 0; i<10; i++)
+	    	{
+	    		var tags = $scope.photodata[i].tags;
+		    	console.log(tags);
+		    	tags_set.push($scope.selectTags(tags));
+	    	}
+	    	console.log(tags_set);
 	    });
   	} 
   	$scope.getPhotos();	
+
+  	$scope.selectTags = function(data) {
+  		var data = data;
+  		var tags = data.split(/\b\s+(?!$)/);
+	   	return tags;
+
+  	}
+
+
 }]);
